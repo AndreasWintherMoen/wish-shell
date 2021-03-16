7acea80d393370d1dd53c50fcb92bcbc5d76c885
AndreasWintherMoen <andreas@winthermoen.no>
2021-03-16 15:02:49 +0100

    Split input string into Commands struct
    Adding a custom terminator was problematic when we tried to use execvp instead of execlp, because the terminator was added as an argument

--- wish.c
+++ wish.c
@@ -3,8 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
-const char terminator[] = "\vEOL\v";
+struct Commands {
+  char **commands;
+  int numCommands;
+};
 
 char** createArray(int m, int n) {
   char **arr=(char **)malloc(sizeof(char *)*m);
@@ -21,6 +25,27 @@ void destroyArray(char** arr)
   free(*arr);
   free(arr);
 }
+
+char *trimwhitespace(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator character
+  end[1] = '\0';
+
+  return str;
+}
+
 
 int stringsAreEqual(const char first[], const char second[]) {
   return (!strcmp(first, second));
@@ -45,46 +70,44 @@ int count(const char string[], const char symbols[]) {
   return output;
 }
 
-void **appendTerminator(char **strings, int numStrings) {
-  char tmp[strlen(terminator)];
-  strcpy(tmp, terminator);
-  strings[numStrings] = tmp;
-}
-
 // Splits a string into a list of strings
-char **split(const char string[], const char delimit[]) {
-  int numWords = count(string, delimit) + 1;
-  char **output = createArray(numWords + 1, 10);
+struct Commands split(const char string[], const char delimit[]) {
+  int numWords = count(string, delimit) + 1; // 1 + number of delimiters
+  char **commands = createArray(numWords, 10);
   int j = 0;
   int count = 0;
   for (int i = 0; i < strlen(string); i++) {
     // if delimiter, append \0 to finish word, and then start a new word
     if(contains(delimit, string[i])) {
-      output[count][j]='\0';
+      commands[count][j]='\0';
       count++;
       j=0;
     }
     // else just add the char to the current word
     else
     {
-      output[count][j]=string[i];
+      commands[count][j]=string[i];
       j++;
     }
   }
-  appendTerminator(output, numWords);
+  struct Commands output;
+  output.commands = commands;
+  output.numCommands = numWords;
   return output;
 }
 
-void printWords(char **words) {
-  for (int i = 0; !stringsAreEqual(words[i], terminator); i++) { 
-    printf(" %s", words[i]);
+void printWords(struct Commands words) {
+  printf("%s", words.commands[0]);
+  for (int i = 1; i < words.numCommands; i++) { 
+    printf(", %s", words.commands[i]);
   }
+  printf("\n");
 }
 
-void executeCommand(char **input) {
+void executeCommand(struct Commands input) {
   int pid = fork();
   if (pid == 0) {
-    execlp(input[0], input[0], NULL);
+    execvp(input.commands[0], input.commands);
   }
   int *foo;
   wait(foo);
@@ -96,8 +119,9 @@ void main(int argc, char **argv) {
   while (1) {
     printf("> ");
     fgets(input, sizeof(input), stdin);
-    char **words = split(input, delimiter);
-    executeCommand(words);
-    destroyArray(words);
+    trimwhitespace(input);
+    struct Commands commands = split(input, delimiter);
+    executeCommand(commands);
+    destroyArray(commands.commands);
   }
 }
